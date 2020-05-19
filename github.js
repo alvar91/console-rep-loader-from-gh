ab17873fbea014756af7f63893e2372d67fb8dc5
Aleksey <aleksey91scorp@bk.ru>
2020-05-19 14:34:47 +0300

    response render added

--- github.js
+++ github.js
@@ -1,13 +1,19 @@
 const https = require("https");
 
-function getRepos(username, done) {
+function getRepos(username = "alvar91", done) {
   const options = {
     hostname: "api.github.com",
     path: `/users/${username}/repos`,
-    headers: { "User-Agent": "alvar91" },
+    headers: { "User-Agent": `${username}` },
   };
   https.get(options, (res) => {
-    console.log(res.statusMessage);
+    res.setEncoding("utf-8");
+    let body = "";
+    res.on("data", (data) => (body += data));
+    res.on("end", () => {
+      const result = JSON.parse(body);
+      done(null, result);
+    });
   });
 }
 
