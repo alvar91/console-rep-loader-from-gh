233f64c6a0671b2a2ec7fd8cc31a2e4822dc0603
Aleksey <aleksey91scorp@bk.ru>
2020-05-19 14:57:15 +0300

    error processing added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const github = require("./github");
 const username = process.argv[2];
 
 github.getRepos(username, (error, repos) => {
-  if (error) console.error(`Ошибка: ${error.message}`);
-
-  repos.forEach((repo) => console.log(repo.name));
+  if (error) {
+    console.error(`Ошибка: ${error.message}`);
+  } else {
+    repos.forEach((repo) => console.log(repo.name));
+  }
 });
--- github.js
+++ github.js
@@ -1,20 +1,39 @@
 const https = require("https");
 
-function getRepos(username = "alvar91", done) {
+function getRepos(username, done) {
+  if (!username) return done(new Error("Необходимо указать имя пользователя"));
+
   const options = {
     hostname: "api.github.com",
     path: `/users/${username}/repos`,
     headers: { "User-Agent": `${username}` },
   };
-  https.get(options, (res) => {
+
+  const req = https.get(options, (res) => {
     res.setEncoding("utf-8");
-    let body = "";
-    res.on("data", (data) => (body += data));
-    res.on("end", () => {
-      const result = JSON.parse(body);
-      done(null, result);
-    });
+    if (res.statusCode === 200) {
+      let body = "";
+      res.on("data", (data) => (body += data));
+      res.on("end", () => {
+        try {
+          const result = JSON.parse(body);
+          done(null, result);
+        } catch (error) {
+          done(new Error(`Не удалось обработать данные ${error.message}`));
+        }
+      });
+    } else {
+      done(
+        new Error(
+          `Не удалось получить данные от сервера (${res.statusCode}) ${res.statusMessage}`
+        )
+      );
+    }
   });
+
+  req.on("error", (error) =>
+    done(new Error(`Не удалось отправить запрос: ${error.message}`))
+  );
 }
 
 module.exports = {
